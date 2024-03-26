c690657327bcbdd89fb81d62fee6d7364ab23c41
Dirk Johnson <80490948+DirkJohnson4@users.noreply.github.com>
2024-03-26 15:55:22 -0400

    Add files via upload

---
+++ lee county executable/New folder/leetcode#1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+    for (int i = 0; i < nums.len; i++); { 
+        if num[i] + nums[i + 1] = target{
+            std::cout << "[" << i << "," << i+1 << "]" << endl;
+        }
+    }
+};
